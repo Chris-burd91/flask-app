732d43ce499e883507d307051cb0eb4b5b241bcc
Chris-burd91 <chrisburd91@gmail.com>
2020-07-24 23:00:20 +0000

    uploading to my flask repository

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,17 @@
 # import Flask class from the flask module
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import getenv
 # create a new instance of Flask and store it in app 
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] =str(getenv('SECRET_KEY'))
 # import the ./application/routes.py file
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
+db = SQLAlchemy(app)
+
+
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,26 +1,16 @@
 # import render_template function from the flask module
-from flask import render_template
+from flask import render_template, redirect, url_for
 # import the app object from the ./application/__init__.py
-from application import app
+from application import app, db
 # define routes for / & /home, this function will be called when these are accessed
-
-blogData = [
-    {  
-        "name": {"first":"Tobi", "last":"Jackson"},
-        "title":"First Post",
-        "content":"My first blog post about Flask"
-    },
-    {   
-        "name": {"first":"Tobias", "last":"Jackson"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
+from application.models import Posts
+from application.forms import PostForm
 
 @app.route('/')
 @app.route('/home')
 def home():
- return render_template('home.html', title='Home', posts=blogData)
+ postData = Posts.query.all()
+ return render_template('home.html', title='Home',post=postData)
 
 
 @app.route('/about')
@@ -36,3 +26,24 @@ def login():
 @app.route('/register')
 def register():
  return render_template('register.html', title='Register')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
