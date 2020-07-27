1423476ed949cc0053891c4bead8e27b4c2b41fd
Chris-burd91 <chrisburd91@gmail.com>
2020-07-27 16:56:09 +0000

    flask exercises and tutorials to this point

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from os import getenv
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 # create a new instance of Flask and store it in app 
 
 app = Flask(__name__)
@@ -13,5 +15,8 @@ app.config['SECRET_KEY'] =str(getenv('SECRET_KEY'))
 app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
 
+bcrypt = Bcrypt(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 from application import routes
